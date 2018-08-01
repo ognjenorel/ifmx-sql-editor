1c43c41b3ef432b4502376e3e17a96fda372f0fd
Ognjen Orel <ognjenorel@users.noreply.github.com>
2018-08-01 15:20:48 +0200

    Display images in SQL results

--- src/ifmxsqleditor/ui/CellDetailsWindow.groovy
+++ src/ifmxsqleditor/ui/CellDetailsWindow.groovy
@@ -1,4 +1,4 @@
-// Copyright 2017 Ognjen Orel
+// Copyright 2018 Ognjen Orel
 //
 // This file is part of IFMX SQL Editor.
 //
@@ -18,9 +18,17 @@
 package ifmxsqleditor.ui
 
 import groovy.swing.SwingBuilder
+
+import javax.imageio.ImageIO
+import javax.imageio.stream.ImageInputStream
+import javax.swing.ImageIcon
+import javax.swing.JLabel
 import javax.swing.JScrollPane
+import java.awt.Dimension
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
+import java.awt.image.BufferedImage
+import java.awt.image.Raster
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,19 +57,37 @@ class CellDetailsWindow {
          ] as KeyListener
 
          vbox {
-            scrollPane(horizontalScrollBarPolicy: JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED,
-                       verticalScrollBarPolicy: JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED) {
+            sp1 = scrollPane(horizontalScrollBarPolicy: JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED,
+                       verticalScrollBarPolicy: JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                       visible: true) {
                ta = swing.textArea(text: '', rows: 18, columns: 40, editable: false, autoscrolls: true, lineWrap: true, wrapStyleWord: true)
+            }
+            sp2 = scrollPane(horizontalScrollBarPolicy: JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED,
+                       verticalScrollBarPolicy: JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                       visible: false) {
+               im = swing.label( )
             }
          }
       }
       frame.pack()
       swing.dataDetailsFrame.addKeyListener keyListener
       swing.ta.addKeyListener keyListener
+      swing.im.addKeyListener keyListener
    }
 
    void show(def data, def parent) {
-      swing.ta.text = data
+
+      if (data instanceof byte[] && ResultSetTableModel.isImage(data)) {
+         swing.im.setIcon(new ImageIcon(data))
+         swing.im.resize(swing.im.icon.iconWidth, swing.im.icon.iconHeight)
+         swing.sp1.setVisible false
+         swing.sp2.setVisible true
+      }
+      else {
+         swing.ta.text = data
+         swing.sp1.setVisible true
+         swing.sp2.setVisible false
+      }
       //GUIUtils.centerOnScreen swing.dataDetailsFrame
       swing.dataDetailsFrame.setLocationRelativeTo parent
       swing.dataDetailsFrame.setVisible true
